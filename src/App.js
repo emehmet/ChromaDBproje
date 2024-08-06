c154f571379f00f7aa6fd4bb4b4575eda0cc5fde
Mertcan Çalışkan <mertcancaliskan@gmail.com>
2024-08-06 11:15:09 +0300

    commit
    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import QueryComponent from './Components/QueryComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ChromaDB Query Interface</h1>
+      <QueryComponent />
+    </div>
+  );
+}
+
+export default App;
+
