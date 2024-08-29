bb8c504dd4edef5c6537111a1f62ddd6aae267c1
ikzayro <admnsts@hotmail.com>
2024-08-29 17:31:24 +0300

    changes on readData.ts

--- backend/readData.ts
+++ backend/readData.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import axios from 'axios';
 import FormData from 'form-data';
+import path from 'path';
 
 
 interface JsonObject {
@@ -20,11 +21,13 @@ async function uploadFile(filePath: string) {
     const form = new FormData();
 
     form.append('file', fs.createReadStream(filePath));
+
+    form.append('fast', 'true');
     
     form.append('types', 'all');
 
     try {
-        const response = await axios.post(/*'http://10.50.17.5:5080'*/'http://localhost:5080', form, {
+        const response = await axios.post(/*'http://10.50.17.5:5060'*/'http://localhost:5080', form, {
             headers: {
                 ...form.getHeaders(),
             },
@@ -39,98 +42,150 @@ async function uploadFile(filePath: string) {
 }
 
 // Function to read JSON from a .txt file and extract text of type 'Text'
-export async function filterDataOld(filePath: string): Promise<[string[], string[]]> {
+// export async function filterDataOld(filePath: string): Promise<[string[], string[]]> {
 
-    const data = await uploadFile(filePath);
-    //console.log(data);
+//     const data = await uploadFile(filePath);
+//     //console.log(data);
 
-    // Read the file content as a string
-    /*const fileContent = fs.readFileSync('C:/Users/ceyhun.erdonmez/Desktop/pdfs/Akkök Holding Sosyal Medya Politikası1.txt', 'utf-8');
+//     // Read the file content as a string
+//     /*const fileContent = fs.readFileSync('C:/Users/ceyhun.erdonmez/Desktop/pdfs/Akkök Holding Sosyal Medya Politikası1.txt', 'utf-8');
 
-    // Parse the string content into a JSON array
-    const jsonArray: JsonObject[] = JSON.parse(fileContent);*/
+//     // Parse the string content into a JSON array
+//     const jsonArray: JsonObject[] = JSON.parse(fileContent);*/
 
-    // Filter the array to only include items where the type is 'Text'
-    const textArray = data
-        .filter((item: { type: string; top: any; page_height: any; }) => item.type === 'Text' || 
-            item.type === 'List item' || 
-            item.type === 'Section header' || 
-            item.type === 'Formula'
-            ||
-            (item.type === 'Table' && 
-            (Number(item.top) <= Number(item.page_height)*0.85 && 
-            Number(item.top) >= Number(item.page_height)*0.10))
-        )
-        .map((item: { text: any; type: any; }) => item.text + "=>" + item.type);
+//     // Filter the array to only include items where the type is 'Text'
+//     const textArray = data
+//         .filter((item: { type: string; top: any; page_height: any; }) => item.type === 'Text' || 
+//             item.type === 'List item' || 
+//             item.type === 'Section header' || 
+//             item.type === 'Formula'
+//             ||
+//             (item.type === 'Table' && 
+//             (Number(item.top) <= Number(item.page_height)*0.83 && 
+//             Number(item.top) >= Number(item.page_height)*0.10))
+//         )
+//         .map((item: { text: any; type: any; }) => item.text + "=>" + item.type);
 
-    const metaArray = data
-        .filter((item: { type: string; top: any; page_height: any; }) => (item.type === 'Table' && 
-            (Number(item.top) > Number(item.page_height)*0.85 || 
-            Number(item.top) < Number(item.page_height)*0.10))
-            ||
-            item.type === 'Page footer' ||
-            item.type === 'Page header' ||
-            item.type === 'Footnote' ||
-            item.type === 'Caption')
-        .map((item: { text: any; type: any; }) => item.text + "=>" + item.type);
+//     const metaArray = data
+//         .filter((item: { type: string; top: any; page_height: any; }) => (item.type === 'Table' && 
+//             (Number(item.top) > Number(item.page_height)*0.83 || 
+//             Number(item.top) < Number(item.page_height)*0.10))
+//             ||
+//             item.type === 'Page footer' ||
+//             item.type === 'Page header' ||
+//             item.type === 'Footnote' ||
+//             item.type === 'Caption')
+//         .map((item: { text: any; type: any; }) => item.text + "=>" + item.type);
     
-    console.log('DOCUMENTS')
-    console.log(textArray);
-    console.log('METADATA')
-    console.log(metaArray);
-    return [textArray, metaArray];
-}
+//     console.log('DOCUMENTS')
+//     console.log(textArray);
+//     console.log('METADATA')
+//     console.log(metaArray);
+//     return [textArray, metaArray];
+// }
 
 export async function filterData(filePath: string): Promise<Object[]> {
-    const data = await uploadFile(filePath);
+    console.log('filterData fonksiyonu çağrıldı.')
 
+    const data = await uploadFile(filePath);
+    console.log(data);
     const array = [];
+    const metaArray = []; metaArray.push(null);
     let currentTextSection: string = ``;
     let currentHeaderSection: string = '';
     let currentFooterSection: string = '';
-    let previousType: string = '';
+    let currentTableSection: string = '';
+    let previousType: string = 'Section header';
     let seeHeader = false;
+    let headerPageNumber = 1;
+
+    let metaPageNumber = 1;
+    for (const item of data){
+        if(item.page_number > metaPageNumber){
+            metaArray.push(
+                {
+                    pageNumber: metaPageNumber,
+                    header: currentHeaderSection,
+                    footer: currentFooterSection,
+                    tables: currentTableSection,
+                    source: path.basename(filePath)
+                }
+            );
+            metaPageNumber = item.page_number;
+            currentFooterSection = '';
+            currentHeaderSection = '';
+            currentTableSection = '';
+        }
+
+        if ((item.type === 'Table' && Number(item.top) > Number(item.page_height)*0.83) ||
+            item.type === 'Page footer') {
+                currentFooterSection = currentFooterSection + item.text + ' ';
+        }
+        else if ((item.type === 'Table' && Number(item.top) < Number(item.page_height)*0.10) ||
+            item.type === 'Page header') {
+                currentHeaderSection = currentHeaderSection + item.text + ' ';
+        }
+        else if (item.type === 'Table' && 
+            (Number(item.top) <= Number(item.page_height)*0.83 && 
+            Number(item.top) >= Number(item.page_height)*0.10)){
+                currentTableSection = currentTableSection + item.text.replace(/\s{3,}/g, ' ') + ' - ';
+        }
+
+    }
+
+    metaArray.push(
+        {
+            pageNumber: metaPageNumber,
+            header: currentHeaderSection,
+            footer: currentFooterSection,
+            tables: currentTableSection,
+            source: path.basename(filePath)
+        }
+    );
+
 
     for (const item of data) {
-        if (item.type === 'Table' && 
-            (Number(item.top) <= Number(item.page_height)*0.85 && 
-            Number(item.top) >= Number(item.page_height)*0.10)) {
+        //currentTextSection = currentTextSection.replace(/\t+/g, ' ');
+        currentTextSection = currentTextSection.replace(/\s{3,}/g, ' ');
+        const wordInString = (s: string, word: string) => new RegExp('\\b' + word + '\\b', 'i').test(s);
+
+        // if (item.type === 'Table' && 
+        //     (Number(item.top) <= Number(item.page_height)*0.83 && 
+        //     Number(item.top) >= Number(item.page_height)*0.10)) {
+        //         /*array.push(
+        //             {text: item.text.replace(/\s{3,}/g, ' '),
+        //              metadata: metaArray[item.page_number]
+        //             }
+        //         );*/
+        //         let skip = false;
+        //         [
+        //             'form',            
+        //             'formu',           
+        //             'formlar',         
+        //             'formları'
+        //         ].forEach(q => wordInString(item.text, q) == true ? skip = true : null)
+
+        //         if(!skip)
+        //             currentTextSection = currentTextSection + `\n` + item.text;
+        // }
+
+        if ((item.type === 'Section header' && previousType != 'Section header') && currentTextSection !== '' && !(Number(item.top) >= Number(item.page_height)*0.88) ||
+                    (seeHeader === false && currentTextSection !== '' && item.type !== 'List item')){
+            if(currentTextSection.trim().split(/\s+/).length > 15){
                 array.push(
-                    [item.text,
-                    {
-                        pageNumber: item.page_number,
-                        header: currentHeaderSection,
-                        footer: currentFooterSection
-                    }]
+                    {text: currentTextSection,
+                    metadata: metaArray[headerPageNumber]
+                    }
                 );
-        }
-        else if ((item.type === 'Table' && Number(item.top) > Number(item.page_height)*0.85)) {
-                currentFooterSection = item.text;
-                if(item.type === 'Page footer')
-                    currentFooterSection = currentFooterSection + item.text + ' ';
-
-        }
-        else if ((item.type === 'Table' && Number(item.top) < Number(item.page_height)*0.10)) {
-                currentHeaderSection = item.text;
-                if(item.type === 'Page header')
-                    currentHeaderSection = currentHeaderSection + item.text + ' ';
-
-        }
-        else if ((item.type === 'Section header' && previousType != 'Section header') ||
-                    (seeHeader === false && currentTextSection !== '')){
-            array.push(
-                [currentTextSection,
-                {
-                    pageNumber: item.page_number,
-                    header: currentHeaderSection,
-                    footer: currentFooterSection
-                }]
-            );
-            currentTextSection = '';
+                currentTextSection = '';
+                headerPageNumber = item.page_number;
+            }
+                
+            
         }
 
-
-        if (item.type === 'Section header' || item.type === 'Text' || item.type === 'List item' || item.type === 'Formula' || item.type === 'Caption') {
+        if ((item.type === 'Section header' || item.type === 'Text' || item.type === 'List item' || item.type === 'Formula' || item.type === 'Caption') &&
+            !(Number(item.top) >= Number(item.page_height)*0.88)) {
             if(previousType === 'Text' && item.type === 'List item')
                 currentTextSection = currentTextSection + `\n`;
 
@@ -140,18 +195,56 @@ export async function filterData(filePath: string): Promise<Object[]> {
                 seeHeader = true;
             if(item.type === 'Section header' || item.type === 'List item')
                 currentTextSection = currentTextSection + `\n`;
+
+            previousType = item.type;
         }
-        previousType = item.type;
-        console.log(item.text + ' => ' +item.type);
+        
+
+        //console.log(item.text + ' => ' +item.type);
+        console.log(item);
     }
 
-    console.log(array);
+    
+    //console.log(metaArray);
+    
+    if((currentTextSection.localeCompare('') !== 0) && currentTextSection.trim().split(/\s+/).length > 15)
+        array.push(
+            {text: currentTextSection,
+             metadata: metaArray[headerPageNumber]
+            }
+        );
+
+    //console.log(array);
+
+
+    const fileName = path.basename(filePath);
+    fs.writeFileSync(`./filteredPdfs/${fileName}.json`, JSON.stringify(array, null, 2), 'utf-8');
+
+    fs.writeFileSync(`./filteredPdfs/1${fileName}.json`, JSON.stringify(data, null, 2), 'utf-8');
+
     return array;
 }
 
+export function writeToFile(fileName: string, array: Promise<Object[]>) {
+
+    //fs.writeFileSync(`${fileName}.json`, JSON.stringify(array, null, 2).replace(/\\n/g, '\n'), 'utf-8');
+    fs.writeFileSync(`${fileName}.json`, JSON.stringify(array, null, 2), 'utf-8');
+}
+
 // Example usage
-const filePath = 'C:/Users/ceyhun.erdonmez/Desktop/pdfs/AKTEK İZİN PROSEDÜRÜ.pdf';
-const extractedTexts = filterData(filePath);
+// const filePath = 'C:/Users/ceyhun.erdonmez/Desktop/pdfs/Performans ve Performansa Bağlı Prim Sistemi.pdf';
+// const extractedTexts = filterData(filePath);
+//writeToFile('ÇALIŞMA MODELİ PROSEDÜRÜ-', extractedTexts);
+
+
+var files = fs.readdirSync('C:/Users/ceyhun.erdonmez/Desktop/pdfs/');
+
+for (const item of files) {
+    
+    const extractedTexts = filterData(`C:/Users/ceyhun.erdonmez/Desktop/pdfs/${item}`);
+    //writeToFile(item.substring(0, item.length - 4), extractedTexts);
+}
+
 
 // Output the extracted text array
 //console.log(extractedTexts);
